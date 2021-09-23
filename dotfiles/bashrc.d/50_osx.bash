7453e1652a8d081780ec5519a4913fe1d915d3db
Tobias Vollmer <info@tvollmer.de>
2021-09-23 02:59:27 +0200

    Add aliases

--- dotfiles/bashrc.d/50_osx.bash
+++ dotfiles/bashrc.d/50_osx.bash
@@ -13,3 +13,6 @@ alias c="tr -d '\n' | pbcopy"
 
 # Start ScreenSaver. This will lock the screen if locking is enabled.
 alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+
+# Add color output for ls
+alias ls="command ls -G"
